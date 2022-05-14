13137bb8890a4230021ce8034b931db7e50872ca
asaduzzaman-al <asaduzzaman1140@gmail.com>
2022-05-14 14:57:46 +0600

    uplaod all projects files

---
+++ app.js
@@ -0,0 +1,55 @@
+
+/**
+ * Create an email pattern 
+ */
+
+// let txt = "asaduzzaman1140@gmail.com";
+
+// let pattern = /^[a-z0-9\._]*@[a-z0-9-]*\.[a-z]{2,5}$/;
+
+// console.log(pattern.test(txt));
+
+
+/**
+ * Create a  username pattern
+ */
+
+// let txt = "asad_asj29+";
+
+// let pattern = /^[a-z_0-9]{5,12}\+$/;
+
+// console.log(pattern.test(txt));
+
+
+/**
+ * Create a Bangladeshi phone number pattern
+ */
+
+// let txt = "01723744819";
+// let pattern = /^(01|8801|\+8801)[0-9]{9}$/;
+
+// console.log(pattern.test(txt));
+
+
+
+/**
+ * Create a password pattern 
+ */
+
+// let txt = "Asad75#$%^&*@!";
+
+// let pattern = /^[a-zA-Z0-9%$@^&*!#]{6,16}$/;
+
+// console.log(pattern.test(txt));
+
+
+
+/**
+ * Create a zipcode pattern  
+ */
+
+let txt = "1360";
+
+let pattern = /^[0-9]{4}$/;
+
+console.log(pattern.test(txt));
